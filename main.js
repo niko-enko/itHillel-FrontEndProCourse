66cf0924dbcf2d61ca8f95515ecc8e5b0025a916
niko-enko <danikon1314@gmail.com>
2025-02-19 16:24:16 +0200

    Додано змінні та вивід типу даних в консоль

---
+++ main.js
@@ -0,0 +1,14 @@
+let personName = 'Danylo';
+let age = 28;
+let genderFemale = false;
+let haveChild;
+let car = null;
+let ID = Symbol('description');
+
+console.log(typeof personName);
+console.log(typeof age);
+console.log(typeof genderFemale);
+console.log(typeof haveChild);
+console.log(typeof car);
+console.log(typeof something);
+console.log(typeof ID);
