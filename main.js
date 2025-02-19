639569e8075f0d24e2af25790700f734e85e77a5
niko-enko <danikon1314@gmail.com>
2025-02-19 16:51:30 +0200

    Додано поля для вводу та вивід відповідей у консоль

---
+++ main.js
@@ -0,0 +1,5 @@
+let name = prompt('Як вас звуть?');
+let age = prompt('Скільки вам років');
+let sweets = prompt('Ваші улюблені солодощі це...');
+
+console.log(`${name}, якому/якій ${age} років, обожнює смакувати ${sweets}`);
