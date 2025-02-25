cb5d52e7f63e8fe48f08d808ff808e6db5fe3542
niko-enko <danikon1314@gmail.com>
2025-02-25 09:01:22 +0200

    Код розділення на цифри перероблено на універсальний для будь якого 5тизначеного числа

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let number = 10369;
+let number = 21456;
 
-let d1 = number % 2;
-let d2 = number % 1;
+let d1 = ((number % 100000) - (number % 10000)) / 10000;
+let d2 = ((number % 10000) - (number % 1000)) / 1000;
 let d3 = ((number % 1000) - (number % 100)) / 100;
-let d4 = (number % 100) / 10 - 0.9;
+let d4 = ((number % 100) - (number % 10)) / 10;
 let d5 = number % 10;
 
 console.log(`${d1} ${d2} ${d3} ${d4} ${d5}`);
