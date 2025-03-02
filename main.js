68101d488e531835009e88b7c66f8cbdbc0f5d8a
niko-enko <danikon1314@gmail.com>
2025-03-02 21:22:20 +0200

    Створено функцію для вирізання символів з рядку який введе користувач

---
+++ main.js
@@ -0,0 +1,17 @@
+function removeCharFromString(str, charToDelete) {
+	let output = str;
+	for (let i = 0; i < charToDelete.length; i++) {
+		if (!output.includes(charToDelete[i])) {
+			console.log(`Літери ${charToDelete[i]} немає у рядку. Пропускаємо`);
+			continue;
+		}
+
+		output = output.replaceAll(charToDelete[i], '');
+	}
+
+	return output;
+}
+
+let userInput = prompt('Введіть текст').toLowerCase();
+let charsToDel = prompt('Введіть символи (через пробіл) які потрібно видалити').split(' ');
+console.log(removeCharFromString(userInput, charsToDel));
