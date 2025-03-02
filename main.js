1c19a0f87052156c104be7663bbe4765287c7d16
niko-enko <danikon1314@gmail.com>
2025-03-02 19:17:56 +0200

    Зміна умови перевірки. Зміна виводу в консоль

--- main.js
+++ main.js
@@ -5,12 +5,18 @@ if (n < 1) {
 } else if (isNaN(n)) {
 	console.log('Помилка! Вводьте лише числа у поле');
 } else {
-	for (let i = 2; i <= 3; i++) {
-		if (n > 3 && n % i === 0) {
-			console.log(`Число ${n} - непросте`);
+	let isSimple = true;
+
+	for (let i = 2; i <= Math.sqrt(n); i++) {
+		if (n % i === 0) {
+			isSimple = false;
 			break;
-		} else if (i > 2) {
-			console.log(`Число ${n} - просте`);
 		}
+	}
+	
+	if (isSimple) {
+		console.log(`Число ${n} - просте`);
+	} else {
+		console.log(`Число ${n} - непросте`);
 	}
 }
